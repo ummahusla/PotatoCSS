b928603b68641d7638c1d9bdb94a4950dc8efb62
toptalo <spam.vitek@gmail.com>
2017-10-16 14:31:51 +0300

    Move changes to SCSS version

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,64 +1,75 @@
 import gulp from 'gulp';
-import sass from 'gulp-sass';
-import autoprefixer from 'gulp-autoprefixer';
-import concat from 'gulp-concat';
 import sourcemaps from 'gulp-sourcemaps';
+import concat from 'gulp-concat';
 import notifier from 'node-notifier';
 import gutil from 'gulp-util';
 import babel from 'gulp-babel';
 import rename from 'gulp-rename';
+
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import mqpacker from 'css-mqpacker';
+
+import csso from 'gulp-csso';
+
+
 const browserSync = require('browser-sync').create();
 
 gulp.task('js', () => {
-  return gulp.src([
-    './js/**/*.js'
-  ])
-  .pipe(sourcemaps.init({loadMaps: true}))
-  .pipe(concat('potato.min.js'))
-  .pipe(babel({presets: ['es2015'], compact: false, minified: true}).on('error', function(err) {
-    gutil.log(gutil.colors.bold.red('Babel compile error'), err.message);
-    notifier.notify({title: 'Babel compile error',message: err.message});
-    this.emit('end');
-  }))
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('./js/'))
-  .pipe(browserSync.reload({stream: true}));
-})
-
-gulp.task('sass', () => {
-  return gulp.src([
-    './stylesheets/sass/**/*.scss',
-    '!./stylesheets/sass/**/_*.scss'
-  ])
-  .pipe(sourcemaps.init({loadMaps: true}))
-  .pipe(concat('potato.css'))
-  .pipe(autoprefixer('last 2 versions'))
-  .pipe(gulp.dest('./stylesheets/css/'))
-  .pipe(sass({outputStyle: 'compressed'}).on('error', function(err) {
-    gutil.log(gutil.colors.bold.red('Sass compile error'), err.message);
-    notifier.notify({title: 'Sass compile error',message: err.message});
-    this.emit('end');
-  }))
-  .pipe(rename({ extname: '.min.css' }))
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('./stylesheets/css/'))
-  .pipe(browserSync.stream());
-})
-
-gulp.task('watch', () => {
-  // gulp.watch(['./stylesheets/sass/**/*.scss', '!./stylesheets/sass/**/_*.scss'], ['sass'])
-  gulp.watch(['./js/**/*.js'], ['js'])
-  gulp.watch(['./examples/*.html']).on('change', browserSync.reload);
-})
-
-gulp.task('browserSync', () => {
-  browserSync.init({
-    server: {
-      baseDir: "./",
-      index: "/examples/index.html"
-    }
-  });
+	return gulp.src([
+		'./js/**/*.js'
+	])
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(concat('potato.min.js'))
+		.pipe(babel({presets: ['es2015'], compact: false, minified: true}).on('error', function (err) {
+			gutil.log(gutil.colors.bold.red('Babel compile error'), err.message);
+			notifier.notify({title: 'Babel compile error', message: err.message});
+			this.emit('end');
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./js/'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
-// gulp.task('default', ['js', 'sass', 'browserSync', 'watch']);
-gulp.task('default', ['js', 'browserSync', 'watch']);
+
+gulp.task('sass', () => {
+	return gulp.src([
+		'./stylesheets/sass/*.scss'
+	])
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}).on('error', function (err) {
+			gutil.log(gutil.colors.bold.red('Sass compile error'), err.message);
+			notifier.notify({title: 'Sass compile error', message: err.message});
+			this.emit('end');
+		}))
+		.pipe(postcss([
+			autoprefixer(),
+			mqpacker()
+		]))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./stylesheets/css/'))
+		.pipe(csso())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./stylesheets/css/'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('watch', () => {
+	gulp.watch(['./stylesheets/sass/**/*.scss'], ['sass']);
+	gulp.watch(['./js/**/*.js'], ['js']);
+	gulp.watch(['./examples/*.html']).on('change', browserSync.reload);
+});
+
+gulp.task('browserSync', () => {
+	browserSync.init({
+		server: {
+			baseDir: "./",
+			index: "/examples/index.html"
+		}
+	});
+});
+
+gulp.task('default', ['js', 'sass', 'browserSync', 'watch']);
